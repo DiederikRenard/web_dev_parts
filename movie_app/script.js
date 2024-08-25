4106630ace67fe3600b4af4af80107fac6993e43
DiederikRenard <diederik.h.renardel@gmail.com>
2024-08-25 13:53:26 +0200

    slideshow done

--- movie_app/script.js
+++ movie_app/script.js
@@ -1,6 +1,6 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4045fd24cd9ba4d06a84dcb4e12ceff4&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=4045fd24cd9ba4d06a84dcb4e12ceff4&query="'
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${INSERT_API_KEY_HERE}="`
 
 
 const form = document.getElementById('form')
